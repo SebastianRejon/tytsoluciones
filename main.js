4514c0e131fa3c3a32e84d2eb983dd72b2352c8d
Sebastian Francisco Rejon <69229650+SebastianRejon@users.noreply.github.com>
2026-05-05 18:15:33 -0300

    Segunda version
    actualizacion de contenido y formatos

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function toggleMenu() {
   document.querySelector(".nav-menu").classList.toggle("active");
 }
+
+// Cierra el menú en móvil al hacer click en un enlace
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector(".nav-menu").classList.remove("active");
+    });
+});
